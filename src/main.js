39fe93a7ba33fe6fc1642ffa8090ef279377ac13
DanZeta1 <fabrizela067@gmail.com>
2026-04-13 01:37:47 -0600

    rutas

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
-import './styles/main.less'
+import '../styles/main.less'
 
 console.log('¡Hola, OPV Drywall! Este es tu archivo main.js. Aquí puedes agregar cualquier código JavaScript que necesites para tu sitio web.');
